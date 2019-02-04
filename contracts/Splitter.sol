054be38b13400ca5ba9575c400e204ecc2186f7d
JavierAguerri <javieraguerrimoreno@gmail.com>
2019-02-04 23:40:29 +0100

    new version

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -3,55 +3,41 @@ pragma solidity 0.5.0;
 contract Splitter {
     
     address public owner; // Alice
-    address payable public add1;  // Bob
-    address payable public add2;  // Carol
     
-    mapping(address => uint) funds;
+    mapping (address => uint) public funds;
     
     constructor() public {
         owner = msg.sender;
     }
     
-    modifier onlyOwner () {
-        require(msg.sender == owner);
-        _;
-    }
+    event addedFunds (
+        address payable indexed _receiver,
+        uint value
+    );
+    
+    event withdrawnFunds (
+        address payable _requester,
+        uint value
+    );
+    
     modifier address0 (address payable add) {
         require(add != address(0));
         _;
     }
-    
-    // set Bob's address
-    function setAddress1 (address payable _add1) public onlyOwner() address0(_add1) {
-        require(_add1 != add1);
-        add1 = _add1;
-    }
-    
-    function setAddress2 (address payable _add2) public onlyOwner() address0(_add2) {
-        require(_add2 != add2);
-        add2 = _add2;
-    }
-    
-    function addFunds (address receiver, uint amount) private {
-        funds[receiver] += amount;
-    }
 
-    function split() public payable onlyOwner() {
-        if (msg.value == 0) {
-            revert();
-        }
-        addFunds(add1, msg.value/2);
-        addFunds(add2, msg.value/2);
+    function split(address payable receiver1, address payable receiver2) public payable address0(receiver1) address0(receiver2) {
+        require(msg.value != 0);
+        funds[receiver1] += msg.value/2 + msg.value%2;
+        emit addedFunds(receiver1, funds[receiver1]);
+        funds[receiver2] += msg.value/2;
+        emit addedFunds(receiver2, funds[receiver2]);
     }
     
     function withdrawFunds() public {
         uint amount = funds[msg.sender];
-
         require(amount != 0);
-        require(address(this).balance >= amount);
-
         funds[msg.sender] = 0;
         msg.sender.transfer(amount);
+        emit withdrawnFunds(msg.sender, amount);
     }
-    
 }
