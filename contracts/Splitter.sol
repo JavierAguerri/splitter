b41284d2be5c3b0035b62aacfc6af14136aa715a
JavierAguerri <javieraguerrimoreno@gmail.com>
2019-02-11 17:19:21 +0100

    And one version more

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -9,38 +9,38 @@ contract Splitter {
     
     event LogFundsAdded (
         address indexed _receiver1,
-        uint value1,
-        address indexed _receiver2,
-        uint value2
+        uint value1
     );
     
     event LogWithdrawnFunds (
-        address _requester,
-        uint value
+        address indexed _requester,
+        uint indexed value
     );
     
-    modifier addressNotZero (address  add) {
-        require(add != address(0));
-        _;
-    }
-    
     modifier fundsNotZero (address add) {
-        require(funds[msg.sender] != 0);
+        require(funds[add] != 0, "Address has no funds available to withdraw");
         _;
     }
 
-    function split(address receiver1, address receiver2) payable public addressNotZero(receiver1) addressNotZero(receiver2) {
-        require(msg.value != 0);
+    function split(address receiver1, address receiver2) payable public {
+	require(receiver1 != address(0), "Receiving address cannot be zero");
+	require(receiver2 != address(0), "Receiving address cannot be zero");
+        require(msg.value >= 0);
         uint half = msg.value / 2;
         funds[receiver1] += half;
-        funds[receiver2] += msg.value - half;
-        emit LogFundsAdded(receiver1, half, receiver2, msg.value - half);
+	emit LogFundsAdded(receiver1, half);
+        funds[receiver2] += half;
+	emit LogFundsAdded(receiver2, half);
+	if (msg.value %2 != 0) {
+		funds[msg.sender] = msg.value % 2;
+		emit LogFundsAdded(msg.sender, msg.value % 2);
+	}
     }
     
     function withdrawFunds() public fundsNotZero(msg.sender) {
         uint amount = funds[msg.sender];
         funds[msg.sender] = 0;
+	emit LogWithdrawnFunds(msg.sender, amount);
         msg.sender.transfer(amount);
-        emit LogWithdrawnFunds(msg.sender, amount);
     }
 }
--- test/splitter.js
+++ test/splitter.js
@@ -3,19 +3,22 @@ const Splitter = artifacts.require("Splitter");
 contract('Splitter', (accounts) => {
   it('should split the received Ether', async () => {
     const splitterInstance = await Splitter.deployed();
+    const accountSender = accounts[0];
     const accountOne = accounts[1];
     const accountTwo = accounts[2];
     const amount = 5000000000000000000;
 
     await splitterInstance.split(accountOne, accountTwo, {from: account[0], value: amount});
 
+    const fundsSender = await splitterInstance.funds.call(accountSender);
     const fundsOne = await splitterInstance.funds.call(accountOne);
     const fundsTwo = await splitterInstance.funds.call(accountTwo);
 
     const half = amount / 2;
 
     assert.equal(fundsOne.valueOf(), half, "half wasn't in the first account");
-    assert.equal(fundsTwo.valueOf(), amount-half, "the rest wasn't in the first account");
+    assert.equal(fundsTwo.valueOf(), half, "half wasn't in the second account");
+    assert.equal(fundsSender.valueOf(), amount % 2, "remaining wasn't in the sender's account");
   });
   it('should withdraw the funds', async () => {
     const splitterInstance = await Splitter.deployed();
