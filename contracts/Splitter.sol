0a4f64beecaabd9ab503dc473caf7c4abaf61b78
JavierAguerri <javieraguerrimoreno@gmail.com>
2019-04-24 09:24:34 +0200

    Improved tests, small fixes in contract

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -8,39 +8,39 @@ contract Splitter {
     }
     
     event LogFundsAdded (
-        address indexed _receiver1,
-        uint value1
+        address indexed receiver,
+        uint indexed value
     );
     
     event LogWithdrawnFunds (
-        address indexed _requester,
+        address indexed requester,
         uint indexed value
     );
     
     modifier fundsNotZero (address add) {
-        require(funds[add] != 0, "Address has no funds available to withdraw");
+        require(funds[add] > 0, "Address has no funds available to withdraw");
         _;
     }
 
     function split(address receiver1, address receiver2) payable public {
-	require(receiver1 != address(0), "Receiving address cannot be zero");
-	require(receiver2 != address(0), "Receiving address cannot be zero");
-        require(msg.value >= 0);
-        uint half = msg.value / 2;
-        funds[receiver1] += half;
-	emit LogFundsAdded(receiver1, half);
-        funds[receiver2] += half;
-	emit LogFundsAdded(receiver2, half);
-	if (msg.value %2 != 0) {
-		funds[msg.sender] = msg.value % 2;
-		emit LogFundsAdded(msg.sender, msg.value % 2);
-	}
+    	require(receiver1 != address(0), "Receiving address cannot be zero");
+    	require(receiver2 != address(0), "Receiving address cannot be zero");
+        require(msg.value>0, "Amount splitted cannot be zero");
+            uint half = msg.value / 2;
+            funds[receiver1] += half;
+    	emit LogFundsAdded(receiver1, half);
+            funds[receiver2] += half;
+    	emit LogFundsAdded(receiver2, half);
+    	if (msg.value %2 != 0) {
+    		funds[msg.sender] += msg.value % 2;
+    		emit LogFundsAdded(msg.sender, msg.value % 2);
+    	}
     }
     
     function withdrawFunds() public fundsNotZero(msg.sender) {
         uint amount = funds[msg.sender];
         funds[msg.sender] = 0;
-	emit LogWithdrawnFunds(msg.sender, amount);
+	    emit LogWithdrawnFunds(msg.sender, amount);
         msg.sender.transfer(amount);
     }
 }
--- test/splitter.js
+++ test/splitter.js
@@ -1,40 +1,120 @@
 const Splitter = artifacts.require("Splitter");
+const chai = require('chai');
+const BN = require('bn.js');
+chai.use(require('chai-bn')(BN));
 
 contract('Splitter', (accounts) => {
-  it('should split the received Ether', async () => {
-    const splitterInstance = await Splitter.deployed();
-    const accountSender = accounts[0];
-    const accountOne = accounts[1];
-    const accountTwo = accounts[2];
-    const amount = 5000000000000000000;
+  console.log(accounts);
+  const accountSender = accounts[0];
+  const accountOne = accounts[1];
+  const accountTwo = accounts[2];
+  const accountZero = "0x0000000000000000000000000000000000000000";
+  const amountBN = new BN('1000000000000000001',10);
+  const twoBN = new BN ('2',10);
+  const halfBN = amountBN.div(twoBN);
+  const remainingBN = amountBN.mod(twoBN);
+  console.log("Half is: "+halfBN);
+  console.log("Remaining is: "+remainingBN);
+  let splitterInstance;
+  beforeEach('setup contract for each test', async function () {
+        splitterInstance = await Splitter.new();
+  })
+  it('should split the received Ether and correctly withdraw balances', async () => {
+    // split funds
+    await splitterInstance.split(accountOne, accountTwo, {from: accounts[0], value: amountBN});
 
-    await splitterInstance.split(accountOne, accountTwo, {from: account[0], value: amount});
+    // save funds associated to each account at this point
+    const initialFundsSenderBN = await splitterInstance.funds(accountSender);
+    const initialFundsOneBN = await splitterInstance.funds(accountOne);
+    const initialFundsTwoBN = await splitterInstance.funds(accountTwo);
+    console.log("Funds sender: "+initialFundsSenderBN);
+    console.log("Funds one: "+initialFundsOneBN);
+    console.log("Funds two: "+initialFundsTwoBN);
 
-    const fundsSender = await splitterInstance.funds.call(accountSender);
-    const fundsOne = await splitterInstance.funds.call(accountOne);
-    const fundsTwo = await splitterInstance.funds.call(accountTwo);
+    // check the funds are what are supposed to be
+    assert.strictEqual(initialFundsSenderBN.toString(), remainingBN.toString(), "remaining wasn't in the sender's account");
+    assert.strictEqual(initialFundsOneBN.toString(), halfBN.toString(), "half wasn't in the first account");
+    assert.strictEqual(initialFundsTwoBN.toString(), halfBN.toString(), "half wasn't in the second account");
 
-    const half = amount / 2;
+    // save the current account balances to check later 
+    const currentBalanceSender = await web3.eth.getBalance(accountSender);
+    const currentBalanceSenderBN = new BN (currentBalanceSender,10);
+    const currentBalanceOne = await web3.eth.getBalance(accountOne);
+    const currentBalanceOneBN = new BN (currentBalanceOne,10);
+    const currentBalanceTwo = await web3.eth.getBalance(accountTwo);
+    const currentBalanceTwoBN = new BN (currentBalanceTwo,10);
 
-    assert.equal(fundsOne.valueOf(), half, "half wasn't in the first account");
-    assert.equal(fundsTwo.valueOf(), half, "half wasn't in the second account");
-    assert.equal(fundsSender.valueOf(), amount % 2, "remaining wasn't in the sender's account");
+    console.log("Sender account contains: "+currentBalanceSender);
+    console.log("Account 1 contains: "+currentBalanceOne);
+    console.log("Account 2 contains: "+currentBalanceTwo);
+
+    // withdraw funds
+    const receipt0 = await splitterInstance.withdrawFunds({from: accounts[0]});
+    const receipt1 = await splitterInstance.withdrawFunds({from: accounts[1]});
+    const receipt2 = await splitterInstance.withdrawFunds({from: accounts[2]});
+
+    // check the funds were withdrawn
+    const finalFundsSenderBN = await splitterInstance.funds(accountSender);
+    const finalFundsOneBN = await splitterInstance.funds(accountOne);
+    const finalFundsTwoBN = await splitterInstance.funds(accountTwo);
+    assert.strictEqual(finalFundsSenderBN.toString(), "0", 'funds one were not correctly withdrawn');
+    assert.strictEqual(finalFundsOneBN.toString(), "0", 'funds one were not correctly withdrawn');
+    assert.strictEqual(finalFundsTwoBN.toString(), "0", 'funds two were not correctly withdrawn');
+
+    // calculate transaction costs
+    const gasUsed0BN = new BN (receipt0.receipt.gasUsed,10);
+    const tx0 = await web3.eth.getTransaction(receipt0.tx);
+    const gasPrice0BN = new BN (tx0.gasPrice,10);
+    const gasUsed1BN = new BN (receipt1.receipt.gasUsed,10);
+    const tx1 = await web3.eth.getTransaction(receipt1.tx);
+    const gasPrice1BN = new BN (tx1.gasPrice,10);
+    const gasUsed2BN = new BN (receipt2.receipt.gasUsed,10);
+    const tx2 = await web3.eth.getTransaction(receipt2.tx);
+    const gasPrice2BN = new BN (tx2.gasPrice,10);
+    //console.log(`GasUsed: ${gasUsed0BN}`);
+    //console.log(`GasPrice: ${gasPrice0BN}`);
+
+    // get actual final balances
+    const finalBalanceSender = await web3.eth.getBalance(accountSender);
+    const finalBalanceSenderBN = new BN (finalBalanceSender,10);
+    const finalBalanceOne = await web3.eth.getBalance(accountOne);
+    const finalBalanceOneBN = new BN (finalBalanceOne,10);
+    const finalBalanceTwo = await web3.eth.getBalance(accountTwo);
+    const finalBalanceTwoBN = new BN (finalBalanceTwo,10);
+
+    //console.log("Sender account contains in the end: "+finalBalanceSender);
+
+    // calculate how much should be left on the accounts
+    const calcBalanceSenderBN = currentBalanceSenderBN.sub(gasUsed0BN.mul(gasPrice0BN)).add(initialFundsSenderBN);
+    const calcBalanceOneBN = currentBalanceOneBN.sub(gasUsed1BN.mul(gasPrice1BN)).add(initialFundsOneBN);
+    const calcBalanceTwoBN = currentBalanceTwoBN.sub(gasUsed2BN.mul(gasPrice2BN)).add(initialFundsTwoBN);
+    assert.strictEqual(calcBalanceSenderBN.toString(), finalBalanceSenderBN.toString(), 'Sender account balance does not match the expected');
+    assert.strictEqual(calcBalanceOneBN.toString(), finalBalanceOneBN.toString(), 'Account 1 balance does not match the expected');
+    assert.strictEqual(calcBalanceTwoBN.toString(), finalBalanceTwoBN.toString(), 'Account 2 balance does not match the expected');
   });
-  it('should withdraw the funds', async () => {
-    const splitterInstance = await Splitter.deployed();
-    const accountOne = accounts[1];
-    const accountTwo = accounts[2];
-    const amount = 5000000000000000000;
 
-    await splitterInstance.split(accountOne, accountTwo, {from: account[0], value: amount});
-
-    await splitterInstance.withdrawFunds({from: account[1]});
-    await splitterInstance.withdrawFunds({from: account[2]});
-
-    const fundsOne = await splitterInstance.funds.call(accountOne);
-    const fundsTwo = await splitterInstance.funds.call(accountTwo);
-
-    assert.equal(fundsOne, 0, 'funds one were not correctly withdrawn');
-    assert.equal(fundsTwo, 0, 'funds two were not correctly withdrawn');
+  it('should fail when sending 0', async () => {
+    let wasError = false;
+    try {
+      await splitterInstance.split(accountOne, accountTwo, {from: accounts[0], value: 0});
+    }
+    catch (err) {
+      wasError = true;
+      console.log(err);
+    }
+    assert.ok(wasError, "splitter did accept a 0 value");
   });
+
+  it('should fail splitting to accounts 0', async () => {
+    let wasError = false;
+    try {
+      await splitterInstance.split(accountOne, accountZero, {from: accounts[0], value: amount});
+    }
+    catch (err) {
+      wasError = true;
+      console.log(err);
+    }
+    assert.ok(wasError, "splitter did accept a 0 address");
+  });
+
 });
